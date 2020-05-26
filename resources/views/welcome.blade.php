ca8fbb064312bcb39412297f7491d0ff6dc6b9f2
Jake Suellentrop <jake@lifeboatcreative.com>
2020-05-25 21:20:52 -0500

    Update app name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name') }}</title>
+        <title>Jake Suellentrop</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    {{ config('app.name') }}
+                    Jake Suellentrop
                 </div>
 
                 <div class="links">
