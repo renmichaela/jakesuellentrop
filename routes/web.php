2e659d5f36530908e1690c909bd2851bda82d4bd
Jake Suellentrop <jake@lifeboatcreative.com>
2020-05-28 11:09:35 -0500

    move page templates

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'welcome')->name('welcome');
+Route::view('/', 'pages.welcome')->name('welcome');
 
-Route::view('/chat', 'chat')->name('chat');
+Route::view('/chat', 'pages.chat')->name('chat');
