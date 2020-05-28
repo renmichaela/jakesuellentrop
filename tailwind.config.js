a8401275cfb562388e2f34ee5a5bdd5828897207
Jake Suellentrop <jake@lifeboatcreative.com>
2020-05-28 08:47:17 -0500

    add purgeCss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-    purge: [],
+    purge: {
+        enabled: true,
+        content: ["./resources/views/*.php", "./resources/js/components/*.vue"]
+    },
     theme: {
         extend: {
             spacing: {
